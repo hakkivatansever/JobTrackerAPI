b5ee7aed2490bbe46bfa0269ddfa603e72ac95a2
Hakka <hakkahalmiev@gmail.com>
2026-04-23 18:42:28 +0100

    feat : add 4 endpoints

--- Controllers/JobsController.cs
+++ Controllers/JobsController.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics.Contracts;
 using JobTrackerAPI.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -25,10 +26,57 @@ public class JobsController : ControllerBase
       }
     };
 
-
+    
     [HttpGet]
     public IActionResult GetAllJobs()
     {
         return Ok(_jobs);
+    }
+
+    // Single Post 
+
+    [HttpGet("{id}")]
+    public IActionResult GetJobById(int id)
+    {
+        var job = _jobs.FirstOrDefault(j => j.Id == id);
+        if (job == null) return NotFound("İlan bulunamadı.");
+        return Ok(job);
+    }
+
+    // Add New Post 
+    [HttpPost]
+    public IActionResult CreateJob([FromBody] Job newJob)
+    {
+        newJob.Id = _jobs.Count + 1;
+        _jobs.Add(newJob);
+        return CreatedAtAction(nameof(GetJobById), new {id = newJob.Id}, newJob);
+
+    }
+
+    // Update Post
+    [HttpPut("{id}")]
+    public IActionResult UpdateJob(int id, [FromBody] Job updatedJob)
+    {
+        var job = _jobs.FirstOrDefault(j => j.Id == id);
+        if(job == null) return NotFound("İlan bulunamadı.");
+
+        job.CompanyName = updatedJob.CompanyName;
+        job.Position = updatedJob.Position;
+        job.Status = updatedJob.Status;
+        job.Notes = updatedJob.Notes;
+
+        return Ok(job);
+    }
+
+    // Delete Post
+
+     [HttpDelete("{id}")]
+    public IActionResult DeleteJob(int id)
+    {
+        var job = _jobs.FirstOrDefault(j => j.Id == id);
+        if (job == null) return NotFound("İlan bulunamadı.");
+
+        _jobs.Remove(job);
+        return NoContent();
     }
 }
