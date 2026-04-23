81b45ea689fce770e3536d537dd22c80c7c22b2a
Hakka <hakkahalmiev@gmail.com>
2026-04-23 21:13:06 +0100

    feat: add sqllite database and migration

--- Program.cs
+++ Program.cs
@@ -1,8 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using JobTrackerAPI.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//Sqlite Connection
+
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlite("Data Source = jobtracker.db") );
 
 var app = builder.Build();
 
